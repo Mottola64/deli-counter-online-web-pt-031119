70c3d7155aa1747048eee37503a096b139ebc398
Michael Mottola <mottola64@gmail.com>
2019-03-21 23:43:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,9 @@
 katz_deli = []
 
-def line(customer)
+def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts ""
-  
+    puts "The line is currently: #{katz_deli}"
+  end
+end
