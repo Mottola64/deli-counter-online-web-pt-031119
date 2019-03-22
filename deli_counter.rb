4467ffe21b4ba56f2d183a56758f5ffb8d0e332f
Michael Mottola <mottola64@gmail.com>
2019-03-22 00:11:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index |person, index|
-    print "The line is currently: #{index+1}. #{person}"
+    puts "The line is currently: #{index+1}. #{person}"
   end
 end
 end
