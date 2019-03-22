a7c2679252a0e1aabc35181847a691e29c55ad8b
Michael Mottola <mottola64@gmail.com>
2019-03-22 00:17:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,5 @@
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(customer_array)
+  if customer_array.count == 0
     puts "The line is currently empty."
-  else
-    katz_deli.each_with_index |person, index|
-    puts "The line is currently: #{index +1}. #{person}"
-  end
-end
-end
-
-def line(line)
- print "The line is currently: "
- line.each_with_index do |person, index|
-   print "#{index+1}. #{person} "
- end
